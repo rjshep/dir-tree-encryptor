5d311ae82db319850f8998d39fb2f9fcc3828372
Richard Shepherd <rjs@tauon.tk>
2016-11-10 18:41:29 +0000

    Finish tests and implementation for PGP encrypt and decrypt

--- src/main/scala/uk/co/fibratus/dte/PGP.scala
+++ src/main/scala/uk/co/fibratus/dte/PGP.scala
@@ -1,17 +1,25 @@
 package uk.co.fibratus.dte
 
 import java.io._
+import java.security.SecureRandom
+import java.util.Date
 
-import org.bouncycastle.openpgp.{PGPOnePassSignatureList, _}
+import org.bouncycastle.bcpg.{CompressionAlgorithmTags, HashAlgorithmTags, SymmetricKeyAlgorithmTags}
 import org.bouncycastle.openpgp.jcajce.JcaPGPObjectFactory
-import org.bouncycastle.openpgp.operator.bc.{BcKeyFingerprintCalculator, BcPBESecretKeyDecryptorBuilder, BcPGPDigestCalculatorProvider, BcPublicKeyDataDecryptorFactory}
-import org.bouncycastle.openpgp.operator.jcajce.{JcaPGPContentSignerBuilder, JcaPGPContentVerifierBuilderProvider}
+import org.bouncycastle.openpgp.operator.bc._
+import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentVerifierBuilderProvider
+import org.bouncycastle.openpgp.{PGPCompressedData, PGPOnePassSignatureList, _}
 
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by rjs on 02/11/2016.
   */
+
+case class Context(pubKeyRingList: List[PGPPublicKeyRing], factory: PGPObjectFactory, outputStream: OutputStream, onePassSignature: Option[PGPOnePassSignature])
+
 object PGP {
 
   def publicKeyringsFromCollection(file: String, userId: Option[String] = None): List[PGPPublicKeyRing] = {
@@ -40,57 +48,112 @@ object PGP {
     it.toList
   }
 
-  def decrypt(encStream: InputStream, decStream: OutputStream, secKeyRing: PGPSecretKeyRing, pass: Array[Char], checkSig: Boolean = false) = {
+  def decrypt(encStream: InputStream, clearStream: OutputStream, pubKeyRingList: List[PGPPublicKeyRing], secKeyRing: PGPSecretKeyRing, pass: String, checkSig: Boolean = false): Try[Unit] = {
+    try {
 
-    val edo = getEncryptedDataObject(encStream)
+      val edo = getEncryptedDataObject(encStream)
 
-    val secKey = secKeyRing.getSecretKey(edo.getKeyID)
+      val secKey = secKeyRing.getSecretKey(edo.getKeyID)
 
-    if (secKey == null)
-      throw new IllegalArgumentException("File was not encrypted to the public key associated with the supplied private key")
+      if (secKey == null)
+        throw new PGPException("File was not encrypted to the public key associated with the supplied private key")
 
-    val privKey = secKey.extractPrivateKey(new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider()).build(pass))
+      val privKey = secKey.extractPrivateKey(new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider()).build(pass.toCharArray))
 
-    val (dataStream, signatureList) = getDataStreamAndSignatureList(edo, privKey)
+      val clear = edo.getDataStream(new BcPublicKeyDataDecryptorFactory(privKey))
 
-    val sig = if(checkSig) {
-      if (signatureList.isEmpty || signatureList.size == 0) throw new IllegalArgumentException("Signature required but not found")
-      val s = signatureList.get.get(0)
-        s.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), secKey.getPublicKey)
-        Some(s)
+      val plainFact = new JcaPGPObjectFactory(clear)
+      val context = Context(pubKeyRingList, plainFact, clearStream, None)
+
+      processMessage(context)
+      Success()
     }
-    else None
-
-    transformStream(dataStream, decStream, sig)
-
-    if(sig.isDefined) {
-
+    catch {
+      case t: Throwable => Failure(t)
     }
   }
 
+  def encrypt(clearStream: InputStream, encStream: OutputStream, pubKeyRingList: List[PGPPublicKeyRing], secKeyRing: PGPSecretKeyRing, pass: String) = {
+    val publicKey = pubKeyRingList.head.getPublicKey
 
-  private def getDataStreamAndSignatureList(edo: PGPPublicKeyEncryptedData, privKey: PGPPrivateKey): (InputStream, Option[PGPOnePassSignatureList]) = {
-    val pgpFact = new JcaPGPObjectFactory(edo.getDataStream(new BcPublicKeyDataDecryptorFactory(privKey)))
+    val rand = new SecureRandom()
+    rand.setSeed(System.currentTimeMillis())
 
-    val plainFactory = new JcaPGPObjectFactory(pgpFact.nextObject.asInstanceOf[PGPCompressedData].getDataStream)
+    val encryptedDataGenerator = new PGPEncryptedDataGenerator(new BcPGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256).setWithIntegrityPacket(true).setSecureRandom(rand))
+    encryptedDataGenerator.addMethod(new BcPublicKeyKeyEncryptionMethodGenerator(publicKey))
 
-    val sig = plainFactory.nextObject.asInstanceOf[PGPOnePassSignatureList]
+    val compressedOut = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZIP).open(encryptedDataGenerator.open(encStream, new Array[Byte](4096)))
 
-    val dataStream = plainFactory.nextObject.asInstanceOf[PGPLiteralData].getDataStream
+    val signatureGenerator = new PGPSignatureGenerator(new BcPGPContentSignerBuilder(publicKey.getAlgorithm, HashAlgorithmTags.SHA512))
 
-    (dataStream, if(sig == null) None else Some(sig))
+    val privKey = secKeyRing.getSecretKey.extractPrivateKey(new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider()).build(pass.toCharArray))
+
+    signatureGenerator.init(PGPSignature.BINARY_DOCUMENT, privKey)
+    signatureGenerator.generateOnePassVersion(true).encode(compressedOut)
+
+    val finalOut = new PGPLiteralDataGenerator().open(compressedOut, PGPLiteralData.BINARY, "", new Date(), new Array[Byte](4096))
+
+    val bytes = new Array[Byte](4096)
+    Iterator
+      .continually(clearStream.read(bytes))
+      .takeWhile(-1 !=)
+      .foreach(read => {
+        finalOut.write(bytes, 0, read)
+        signatureGenerator.update(bytes, 0, read)
+      })
+
+    finalOut.close()
+    clearStream.close()
+    compressedOut.close()
+    encryptedDataGenerator.close()
+    encStream.close()
   }
 
-  private def transformStream(inStream: InputStream, outStream: OutputStream, sig: Option[PGPOnePassSignature]) = {
+  @tailrec
+  private def processMessage(context: Context): Unit = {
+    context.factory.nextObject() match {
+      case x: PGPCompressedData =>
+        processMessage(context.copy(factory = new JcaPGPObjectFactory(x.getDataStream)))
+      case x: PGPOnePassSignatureList =>
+        processMessage(processOPS(context, x.get(0)))
+      case x: PGPLiteralData =>
+        transformStream(context, x.getInputStream())
+        processMessage(context)
+      case x: PGPSignatureList =>
+        context.onePassSignature.foreach(y => verifySignature(x.get(0), y))
+        processMessage(context)
+      case _ => // message == null so we're done
+        if (context.onePassSignature.isEmpty) {
+          throw new PGPException("No signature found")
+        }
+    }
+  }
+
+  private def processOPS(context: Context, ops: PGPOnePassSignature): Context = {
+    val krl = context.pubKeyRingList
+    val pubKeys = krl.map(_.getPublicKey(ops.getKeyID)).filter(_ != null)
+    if (pubKeys.isEmpty) {
+      throw new PGPException("No public keys match message signer")
+    }
+    ops.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), pubKeys.head)
+    context.copy(onePassSignature = Some(ops))
+  }
+
+  private def verifySignature(signature: PGPSignature, onePassSignature: PGPOnePassSignature) = {
+    if (!onePassSignature.verify(signature))
+      throw new PGPException("Signature verification failed")
+  }
+
+  private def transformStream(context: Context, inStream: InputStream) = {
     val bytes = new Array[Byte](1024)
     Iterator
       .continually(inStream.read(bytes))
       .takeWhile(-1 !=)
       .foreach(read => {
-        outStream.write(bytes,0,read)
-        if(sig.isDefined) sig.get.update(bytes, 0, read)
+        context.outputStream.write(bytes, 0, read)
+        context.onePassSignature.foreach(_.update(bytes, 0, read))
       })
-    outStream.close()
+    context.outputStream.close()
   }
 
   private def getEncryptedDataObject(encStream: InputStream): PGPPublicKeyEncryptedData = {
@@ -103,7 +166,6 @@ object PGP {
 
     val encList = pgpObject.asInstanceOf[PGPEncryptedDataList]
 
-    val edo = encList.getEncryptedDataObjects.next.asInstanceOf[PGPPublicKeyEncryptedData]
-    edo
+    encList.getEncryptedDataObjects.next.asInstanceOf[PGPPublicKeyEncryptedData]
   }
 }
--- src/test/scala/uk/co/fibratus/dte/PGPTest.scala
+++ src/test/scala/uk/co/fibratus/dte/PGPTest.scala
@@ -1,13 +1,22 @@
 package uk.co.fibratus.dte
 
-import java.io.ByteArrayOutputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.security.Security
 
-import org.scalatest.FunSuite
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.openpgp.PGPException
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+import scala.util.{Failure, Success}
 
 /**
   * Created by rjs on 03/11/2016.
   */
-class PGPTest extends FunSuite {
+class PGPTest extends FunSuite with BeforeAndAfterAll {
+
+  override def beforeAll() = {
+    Security.addProvider(new BouncyCastleProvider())
+  }
 
   test("publicKeyringsFromCollection returns all keyrings") {
     val c = PGP.publicKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/pubring.gpg"), None)
@@ -39,7 +48,56 @@ class PGPTest extends FunSuite {
   test("decrypt successfully decrypts a file") {
     val out = new ByteArrayOutputStream()
     val secKey = PGP.secretKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/secring2.gpg"), Some("DTE Test User 2 <dte2@local>"))
-    PGP.decrypt(this.getClass.getResourceAsStream("/files/testfile.txt.gpg"), out, secKey.head, "passwd".toCharArray)
+    val pubKeyRingList = PGP.publicKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/pubring.gpg"), None)
+    PGP.decrypt(this.getClass.getResourceAsStream("/files/testfile.txt.gpg"), out, pubKeyRingList, secKey.head, "passwd")
     assert(out.toString === "testfile.txt contents\n")
+  }
+
+  test("decrypt fails to decrypts a file given incorrect secret key passphrase") {
+    val out = new ByteArrayOutputStream()
+    val secKey = PGP.secretKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/secring2.gpg"), Some("DTE Test User 2 <dte2@local>"))
+    val pubKeyRingList = PGP.publicKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/pubring.gpg"), None)
+    val d = PGP.decrypt(this.getClass.getResourceAsStream("/files/testfile.txt.gpg"), out, pubKeyRingList, secKey.head, "notthepasswd")
+    assert(d.isFailure)
+    d match {
+      case Failure(f) =>
+        assert(f.isInstanceOf[PGPException])
+        assert(f.getMessage === "checksum mismatch at 0 of 20")
+      case Success(u) =>
+    }
+  }
+
+  test("decrypt throws validation exception when unable to verify due to absence of key in public keyring") {
+    val out = new ByteArrayOutputStream()
+    val secKey = PGP.secretKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/secring2.gpg"), Some("DTE Test User 2 <dte2@local>"))
+    val pubKeyRingList = PGP.publicKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/pubring2.gpg"), None)
+
+    val d = PGP.decrypt(this.getClass.getResourceAsStream("/files/testfile.txt.gpg"), out, pubKeyRingList, secKey.head, "passwd")
+    assert(d.isFailure)
+    d match {
+      case Failure(f) =>
+        assert(f.isInstanceOf[PGPException])
+        assert(f.getMessage === "No public keys match message signer")
+      case Success(u) =>
+    }
+  }
+
+  test("encrypt successfully encrypts and signs a message") {
+    val msg = "a test message"
+    val in = new ByteArrayInputStream(msg.getBytes)
+    val out = new ByteArrayOutputStream()
+
+    val secKeyRing = PGP.secretKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/secring2.gpg"), Some("DTE Test User 2 <dte2@local>"))
+    val pubKeyRingList = PGP.publicKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/pubring.gpg"), Some("DTE Test User 1 <dte1@local>"))
+
+    PGP.encrypt(in, out, pubKeyRingList, secKeyRing.head, "passwd")
+
+    val decin = new ByteArrayInputStream(out.toByteArray)
+    val decout = new ByteArrayOutputStream()
+    val decsecKey = PGP.secretKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/secring.gpg"), Some("DTE Test User 1 <dte1@local>"))
+    val decpubKeyRingList = PGP.publicKeyringsFromCollection(this.getClass.getResourceAsStream("/keys/pubring2.gpg"), None)
+    PGP.decrypt(decin, decout, decpubKeyRingList, decsecKey.head, "passwd")
+    assert(decout.toString === msg)
+    println(decout.toString)
   }
 }
